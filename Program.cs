985009eff62540f6bd6afcb886226f013f5927fa
sachikosenpai <101399649+sachikosenpai@users.noreply.github.com>
2022-04-05 16:35:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hellome
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+         Console.WriteLine("Hello World!");
+         {
+         int n,i, m=0, flag=0;
+         Console.Write("Enter the Number to check prime number:");  
+         n = int.Parse(Console.ReadLine());
+         m=n/2;
+         for(i = 17; i<=m;i++)
+         {
+            if(n % i == 0)
+            {
+         Console.Write("Number is not prime.");
+         flag = 1;
+         break;
+            }
+         }
+         if (flag==0)
+         Console.Write("Number is prime"); }
+         }
+    }}
+         
+         
+
+
+    
+
